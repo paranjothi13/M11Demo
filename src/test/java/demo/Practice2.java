dd6acdc675c6855351cefad5e3015c202d9f3bef
asus <asus@Firestone-02>
2026-01-16 17:13:05 +0530

    third commit

--- src/test/java/demo/Practice2.java
+++ src/test/java/demo/Practice2.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class Practice2 {
 	@Test
 public void m2() {
-	System.out.println("hi bro");
+	System.out.println("hi git hub");
+	System.out.println("hello git hub");
 }
 }
